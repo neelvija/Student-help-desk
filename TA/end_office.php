d5a400d1f80dce381dc781e6fae764366bada1ea
JianpingLuke <66133511+JianpingLuke@users.noreply.github.com>
2020-06-24 18:36:18 -0400

    change TA status to offline

---
+++ TA/end_office.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ta_system";
+$em = $_SESSION['email'];
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$que = "UPDATE ta_records SET Is_available = 'Offline' where Email = '$em' ";
+
+
+mysqli_query($conn,$que);
+
+echo "Office hour end!"
+?>
